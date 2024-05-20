dfd22b955549a71652d4e25fd004a803d4525a8d
thiago <thiiagosantos300@gmail.com>
2024-05-20 11:16:17 -0300

    remove: remove grcp

--- tasklogger/src/main/java/com/phonereplay/tasklogger/network/Client.java
+++ tasklogger/src/main/java/com/phonereplay/tasklogger/network/Client.java
@@ -1,12 +1,8 @@
 package com.phonereplay.tasklogger.network;
 
 import com.google.gson.Gson;
-import com.google.protobuf.ByteString;
-import com.phonereplay.tasklogger.Binary;
-import com.phonereplay.tasklogger.BinaryDataServiceGrpc;
 import com.phonereplay.tasklogger.DeviceModel;
 import com.phonereplay.tasklogger.LocalSession;
-import com.phonereplay.tasklogger.TimeLine;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
@@ -18,10 +14,6 @@ import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.util.Set;
-
-import io.grpc.ManagedChannel;
-import io.grpc.ManagedChannelBuilder;
 
 public class Client {
 
@@ -83,48 +75,6 @@ public class Client {
             conn.disconnect();
         } catch (Exception e) {
             e.printStackTrace();
-        }
-    }
-
-    public void sendBinaryData(byte[] binaryData, String sessionId, Set<TimeLine> timeLines) {
-        if (binaryData == null || sessionId == null) {
-            return;
-        }
-        String serverAddress = "10.0.0.106"; //you ip
-        int serverPort = 8000;
-
-        ManagedChannel channel = ManagedChannelBuilder.forAddress(serverAddress, serverPort)
-                .usePlaintext()
-                .maxInboundMessageSize(50 * 1024 * 1024)
-                .build();
-
-        try {
-            BinaryDataServiceGrpc.BinaryDataServiceBlockingStub blockingStub = BinaryDataServiceGrpc.newBlockingStub(channel);
-
-            Binary.BinaryDataRequest.Builder requestBuilder = Binary.BinaryDataRequest.newBuilder()
-                    .setBinaryData(ByteString.copyFrom(binaryData))
-                    .setSessionId(sessionId);
-
-            // Adicionando TimeLines à requisição
-            for (TimeLine tl : timeLines) {
-                Binary.TimeLine.Builder tlBuilder = Binary.TimeLine.newBuilder()
-                        .setCoordinates(tl.getCoordinates())
-                        .setGestureType(tl.getGestureType())
-                        .setTargetTime(tl.getTargetTime());
-                requestBuilder.addTimeLines(tlBuilder);
-            }
-
-            Binary.BinaryDataRequest request = requestBuilder.build();
-            Binary.BinaryDataResponse response = blockingStub.sendBinaryData(request);
-
-            System.out.println("Response from server: " + response);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            if (channel != null && !channel.isShutdown()) {
-                channel.shutdown();
-            }
         }
     }
 }
